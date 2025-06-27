5d5ea567fe2e20038074c9b7dfccdc994c1271c5
hema0987654 <142291949+hema0987654@users.noreply.github.com>
2025-06-27 22:15:24 +0300

    end middel ware

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,28 @@ import express, { Application } from "express";
 import methodOverride from "method-override";
 import path from "path";
 import { fileURLToPath } from "url";
-import helmet, { contentSecurityPolicy } from "helmet";
 import run from "./runserver.js";
 import router from "./router/router.js";
 import ErroMiddleWare from "./Middlewares/erroMiddelware.js";
 import NotFound from "./Middlewares/NotFound.js";
-import dontev from "dotenv";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import {rateLimit} from "express-rate-limit";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app: Application = express();
 
+app.use(morgan("dev"));
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: {
+        error: "Too many requests, please try again later."
+    }
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -25,16 +35,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", router);
 app.use(ErroMiddleWare.handel);
 app.use(NotFound.handel);
-app.use(
-    helmet({
-        contentSecurityPolicy: false,
-        
-        frameguard: {
-            action: "deny",
-        },
-    })
-);
-console.log(process.env.PORT);
-dontev.config();
 
 app.listen(process.env.PORT, () => run(Number(process.env.PORT)));
